d8715ef58bc5ba9eba866f0e0e98b7bb151e6d45
abhishek111222 <abhishekagra6@gmail.com>
2024-08-18 09:02:51 +0100

    Enabled the APIs

---
+++ gcp/dataproc-composer/enable_apis.sh
@@ -0,0 +1,8 @@
+gcloud services enable \
+cloudresourcemanager.googleapis.com \
+iam.googleapis.com \
+composer.googleapis.com \
+dataproc.googleapis.com \
+compute.googleapis.com \
+storage.googleapis.com \
+serviceusage.googleapis.com
